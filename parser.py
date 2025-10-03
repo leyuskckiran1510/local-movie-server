e1932184a75d4c52d0728c0fa7d1511094941db3
leyuskckiran1510 <kdhakal1510@gmail.com>
2025-10-03 19:41:45 +0545

    fix: fix parser not returning matches

--- parser.py
+++ parser.py
@@ -43,6 +43,11 @@ def cmp_attrs(_in: Attr_t, _x: Attr_t):
 class TagParser(HTMLParser):
     searches: list[Query_T] = []
     matches: list[str] = []
+    match_found: bool = False
+    match_content: str = ''
+    match_depths: list[int] = []
+    match_cntn_idx: list[int] = []
+    tag_depth: int = 0
 
     def handle_starttag(self, tag: str, attrs: Attr_t):
         for search in self.searches:
@@ -50,16 +55,44 @@ class TagParser(HTMLParser):
                 continue
             if not cmp_attrs(attrs, search['attr']):
                 continue
-            print('Match Found', tag, attrs)
+            self.match_depths.append(self.tag_depth)
+            self.match_cntn_idx.append(len(self.match_content))
+        if self.match_depths:
+            attr_simple = ' '.join([f'{i}="{j}"' for i, j in attrs])
+            self.match_content += f'<{tag} {attr_simple}>'
+        self.tag_depth += 1
 
-    def handle_endtag(self, tag: str): ...
+    def handle_endtag(self, tag: str):
+        if self.match_depths:
+            self.match_content += f'</{tag}>'
 
-    def handle_data(self, data: str | None): ...
+        if self.tag_depth < 0:
+            return
+        else:
+            self.tag_depth -= 1
+
+        if (
+            self.tag_depth > -1
+            and self.match_depths
+            and self.tag_depth == self.match_depths[-1]
+        ):
+            self.match_depths.pop()
+            frm = self.match_cntn_idx.pop()
+            self.matches.append(self.match_content[frm:])
+
+    def handle_data(self, data: str | None):
+        if self.match_depths and data:
+            self.match_content += data
 
 
 if __name__ == '__main__':
     parser = TagParser()
     parser.searches = [{'tag': 'div', 'attr': [('class', 'card v4 tight')]}]
+    # parser.searches = [{'tag': 'a', 'attr': [('data-media-type', 'person')]}]
     with open('test.html', 'r', encoding='utf-8') as fp:
         parser.feed(fp.read())
-    print(parser.matches)
+    print(len(parser.matches))
+    # for match in parser.matches[:1]:
+    #     print('-' * 10)
+    #     print(match.replace('\n', '').replace('\t', ''))
+    #     print('==' * 10, '\n\n')
